60d4e6b3328eca0bd6bb897c240217cac8238b28
xulouzhe <1435529005@qq.com>
2020-03-04 10:21:21 +0800

    update project charset

--- ao3.py
+++ ao3.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask,render_template,request,redirect,url_for
 from getcontent import GetArticle
 from getdetail import GetDetail
--- items.py
+++ items.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 
 class articleItem(object):
     def __init__(self):
